95427fe57402d7a4010559a13ebb4fdd1a0994a1
ayush-dubey <ayushdubey010@gmail.com>
2017-04-22 00:26:15 +0530

    model update
    model

--- application/models/LoginModel.php
+++ application/models/LoginModel.php
@@ -5,7 +5,7 @@ class LoginModel extends CI_Model
 	
 	public function select_users($data)
 	{
-		$row=$this->db->query("select * from users where username='".$data->username."' and password='".$data->password."'");
+		$row=$this->db->query("select * from users where username='".$data->username."' and password='".$data->password."'and role='".$data->mylist."'");
 		if($row)
 			return $row;
 		else
