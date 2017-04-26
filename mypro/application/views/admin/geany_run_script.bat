70ff736f72baec15026cad6152334f4646e1feb7
ayush-dubey <ayushdubey010@gmail.com>
2017-04-26 15:10:30 +0530

    aszx
    xsxs

--- mypro/application/views/admin/geany_run_script.bat
+++
@@ -1,6 +0,0 @@
-php "admin_page.php"
-
-pause
-del "%0"
-
-pause
--- mypro/application/views/public/thank.php
+++
@@ -1,8 +0,0 @@
-<br>
-<div class="wrapper">
-<div class="container">
-   <div class="jumbotron wide" >
-	<h3>thank you for registering!!!!<h3>
-   </div>
-</div>
-</div>
